fa439d41a9f953e6d7e36a18d1567ed7cabd5fd1
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-04-25 20:56:12 +0330

    [ADD] job count to model admins

--- job/admin.py
+++ job/admin.py
@@ -1,24 +1,38 @@
 from django.contrib import admin
 
 from core.admin import BaseModelAdmin
+from core.utils import remove_exponent_decorator
 from job.models import Job, JobLocation, JobTitle, Company  # noqa
 
 
 @admin.register(Company)
 class CompanyAdmin(BaseModelAdmin):
-    list_display = ["name", "universal_name", "linkedin_id"]
+    list_display = ["name", "universal_name", "linkedin_id", "jobs_count"]
+
+    @remove_exponent_decorator
+    def jobs_count(self, obj):
+        return obj.jobs.count()
 
 
 @admin.register(JobTitle)
 class JobTitleAdmin(BaseModelAdmin):
-    list_display = ["title", "linkedin_id"]
+    list_display = ["title", "linkedin_id", "jobs_count"]
+
+    @remove_exponent_decorator
+    def jobs_count(self, obj):
+        return obj.jobs.count()
 
 
 @admin.register(JobLocation)
 class JobLocationAdmin(BaseModelAdmin):
-    list_display = ["title", "linkedin_geo_id", "flag_emoji"]
+    list_display = ["title", "linkedin_geo_id", "flag_emoji", "jobs_count"]
+
+    @remove_exponent_decorator
+    def jobs_count(self, obj):
+        return obj.jobs.count()
 
 
 @admin.register(Job)
 class JobAdmin(BaseModelAdmin):
-    list_display = ["id", "title", "company", "full_location", "status"]
+    list_display = ["id", "title", "company", "location", "full_location", "status"]
+    list_filter = ["location", "company", "job_titles"]
