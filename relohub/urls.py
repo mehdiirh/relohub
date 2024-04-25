cee5760b53ed5bbe98fb3601318cd3403a7c5938
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-04-25 06:15:43 +0330

    [ADD] media and static URLs

--- relohub/urls.py
+++ relohub/urls.py
@@ -1,6 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
