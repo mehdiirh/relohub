192faba154ca4a93d5f289d523fc5465e5f586e6
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-04-27 01:54:57 +0330

    [UPDATE] make job processing multithreaded

--- job/tasks.py
+++ job/tasks.py
@@ -1,23 +1,27 @@
+from time import sleep
+
 import requests.exceptions
 from celery_singleton import Singleton
+from celery_singleton.exceptions import DuplicateTaskError
+from django.conf import settings
 from django.core.files.base import ContentFile
 from django.utils import timezone
-from linkedin_api.linkedin import default_evade, Linkedin
+from linkedin_api.linkedin import default_evade
 
 from job import JobStatus
-from job.models import JobLocation, JobTitle, Job, Company
+from job.models import JobLocation, JobTitle, Job, Company, JobSkill
 from linkedin.models import LinkedinAccount
 from relohub import celery_app as app
 
 
-def get_client() -> Linkedin:
+def get_least_used_account() -> LinkedinAccount:
     linkedin_account: LinkedinAccount = (
         LinkedinAccount.objects.filter(is_active=True).order_by("last_used").first()
     )
     linkedin_account.last_used = timezone.now()
     linkedin_account.save()
 
-    return linkedin_account.client
+    return linkedin_account
 
 
 def resolve_company(job_data: dict):
@@ -52,82 +56,121 @@ def resolve_company(job_data: dict):
 
 @app.task(
     base=Singleton,
+    unique_on=["account_pk"],
+    raise_on_duplicate=True,
     name="job.search_jobs",
-    autoretry_for=(Exception,),
-    max_retries=3,
-    time_limit=60 * 30,
-    lock_expiry=60 * 30,
+    time_limit=10 * 60,
+    lock_expiry=10 * 60,
 )
-def search_jobs():
-    locations = JobLocation.objects.filter(is_active=True).all()
-    job_titles = JobTitle.objects.filter(is_active=True).all()
+def search_jobs(account_pk: int, location_pk: int, job_title_pk: int):
+    account = LinkedinAccount.objects.get(pk=account_pk)
+    location = JobLocation.objects.get(pk=location_pk)
+    job_title = JobTitle.objects.get(pk=job_title_pk)
 
-    client = get_client()
+    client = account.client
 
-    for location in locations:
-        for job_title in job_titles:
-            jobs = []
-            limit, chunk, offset = 200, 20, 0
-            while True:
-                try:
-                    new_jobs = client.search_jobs(
-                        location_geo_id=location.linkedin_geo_id,
-                        job_title=[job_title.linkedin_id],
-                        offset=offset,
-                        listed_at=24 * 60 * 60,
-                        limit=chunk,
-                    )
-                except requests.exceptions.JSONDecodeError:
-                    default_evade()
-                    continue
+    jobs = []
+    limit, chunk, offset = 200, 20, 0
 
-                jobs.extend(new_jobs)
-                offset += chunk
+    while True:
+        try:
+            new_jobs = client.search_jobs(
+                location_geo_id=location.linkedin_geo_id,
+                job_title=[job_title.linkedin_id],
+                offset=offset,
+                listed_at=24 * 60 * 60,
+                limit=chunk,
+            )
+        except requests.exceptions.JSONDecodeError as e:
+            default_evade()
+            continue
 
-                if any(
-                    [
-                        not new_jobs,
-                        offset >= limit,
-                        len(new_jobs) < chunk,
-                    ]
-                ):
-                    break
+        jobs.extend(new_jobs)
+        offset += chunk
 
-                default_evade()
+        if any(
+            [
+                not new_jobs,
+                offset >= limit,
+                len(new_jobs) < chunk,
+            ]
+        ):
+            break
+        default_evade()
 
-            for _job in jobs:
-                job_id = _job["trackingUrn"].split(":")[-1]
-                title = _job["title"]
-
-                job, job_created = Job.objects.get_or_create(
-                    linkedin_id=job_id,
-                    defaults={
-                        "title": title,
-                        "status": JobStatus.PARTIALLY_PROCEEDED,
-                        "location": location,
-                    },
-                )
-                job.job_titles.add(job_title)
-                job.save()
+    for _job in jobs:
+        job_id = _job["trackingUrn"].split(":")[-1]
+        title = _job["title"]
+        job, job_created = Job.objects.get_or_create(
+            linkedin_id=job_id,
+            defaults={
+                "title": title,
+                "status": JobStatus.PARTIALLY_PROCEEDED,
+                "location": location,
+            },
+        )
+        job.job_titles.add(job_title)
+        job.save()
 
 
 @app.task(
     base=Singleton,
-    name="job.process_jobs",
-    autoretry_for=(Exception,),
-    max_retries=3,
-    time_limit=60 * 20,
-    lock_expiry=60 * 20,
+    name="job.run_search_jobs",
+    time_limit=60 * 60,
+    lock_expiry=60 * 60,
 )
-def process_jobs():
-    jobs = Job.objects.filter(is_active=True, status=JobStatus.PARTIALLY_PROCEEDED)[:50]
+def run_search_jobs():
+    locations = JobLocation.objects.filter(is_active=True).all()
+    job_titles = JobTitle.objects.filter(is_active=True).all()
 
-    client = get_client()
+    pending_task_args = []
+    for location in locations:
+        for job_title in job_titles:
+            account = get_least_used_account()
+            args = (account.pk, location.pk, job_title.pk)
+
+            try:
+                search_jobs.delay(*args)
+            except DuplicateTaskError:
+                pending_task_args.append(args)
+
+    while pending_task_args:
+        for idx, args in enumerate(pending_task_args):
+            try:
+                search_jobs.delay(*args)
+                pending_task_args.pop(idx)
+            except DuplicateTaskError:
+                pass
+
+        sleep(5)
+
+
+@app.task(
+    base=Singleton,
+    unique_on=["account_pk"],
+    raise_on_duplicate=True,
+    name="job.process_jobs",
+    time_limit=10 * 60,
+    lock_expiry=10 * 60,
+)
+def process_jobs(account_pk: int, job_pks: list[int]):
+    account = LinkedinAccount.objects.get(pk=account_pk)
+    jobs = Job.objects.filter(pk__in=job_pks).all()
+
+    client = account.client
 
     for job in jobs:
         while True:
             try:
                 job_data = client.get_job(job.linkedin_id)
+                break
+            except requests.exceptions.JSONDecodeError:
+                default_evade()
+                continue
+
+        while True:
+            try:
+                job_skills = client.get_job_skills(job.linkedin_id)
                 break
             except requests.exceptions.JSONDecodeError:
                 default_evade()
@@ -150,7 +193,85 @@ def process_jobs():
             if workplace_type == "3":
                 job.hybrid = True
 
-        job.status = JobStatus.FULLY_PROCEEDED
-        job.save()
+        # noinspection PyUnboundLocalVariable
+        for skill_data in job_skills["skillMatchStatuses"]:
+            skill = skill_data["skill"]
+            name = skill["name"]
+            linkedin_id = skill["entityUrn"].split(":")[-1]
 
+            skill_object, skill_created = JobSkill.objects.get_or_create(
+                linkedin_id=linkedin_id,
+                defaults={"name": name},
+            )
+            job.job_skills.add(skill_object)
+
+        # key in description: 1 points
+        # key in title: 2 points
+        # key+complement in description: 3 points
+        # key+title in description: 5 points
+        job_contains_keywords = False
+        points = 0
+        for key in settings.LINKEDIN_JOB_DESCRIPTION_KEYWORDS:
+            if key.lower() in job.description.lower():
+                job_contains_keywords = True
+                points += 1
+
+            if key.lower() in job.title.lower():
+                job_contains_keywords = True
+                points += 2
+
+        if job_contains_keywords:
+            for key_complement in settings.LINKEDIN_JOB_DESCRIPTION_KEYWORD_COMPLEMENTS:
+                if key_complement.lower() in job.description.lower():
+                    points += 3
+
+                if key_complement.lower() in job.title.lower():
+                    points += 5
+
+        job.points = points
+
+        if points > 0:
+            job.status = JobStatus.WAITING_FOR_REVIEW
+        else:
+            job.status = JobStatus.REJECTED
+
+        job.save()
         default_evade()
+
+
+@app.task(
+    base=Singleton,
+    name="job.run_process_jobs",
+    time_limit=60 * 60,
+    lock_expiry=60 * 60,
+)
+def run_process_jobs():
+    jobs = (
+        Job.objects.filter(is_active=True, status=JobStatus.PARTIALLY_PROCEEDED)
+        .order_by("created_at")
+        .values_list("pk", flat=True)
+        .distinct()
+    )
+
+    chunk = 30
+    pending_task_args = []
+    for offset in range(0, len(jobs), chunk):
+        sliced_jobs = jobs[offset : offset + chunk]
+
+        account = get_least_used_account()
+        args = (account.pk, sliced_jobs)
+
+        try:
+            process_jobs.delay(*args)
+        except DuplicateTaskError:
+            pending_task_args.append(args)
+
+    while pending_task_args:
+        for idx, args in enumerate(pending_task_args):
+            try:
+                process_jobs.delay(*args)
+                pending_task_args.pop(idx)
+            except DuplicateTaskError:
+                pass
+
+        sleep(5)
