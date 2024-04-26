ca13e7af94d6b00c7f89d3d407a50a8199e63676
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-04-26 22:03:48 +0330

    [UPDATE] Change Accepted -> Listed in job status

--- job/__init__.py
+++ job/__init__.py
@@ -7,6 +7,6 @@ class JobStatus(TextChoices):
     PARTIALLY_PROCEEDED = "PARTIALLY_PROCEEDED", _("Partially Proceeded")
     FULLY_PROCEEDED = "FULLY_PROCEEDED", _("Fully Proceeded")
     WAITING_FOR_REVIEW = "WAIT_FOR_REVIEW", _("Waiting For Review")
-    ACCEPTED = "LISTED", _("Listed")
+    LISTED = "LISTED", _("Listed")
     REJECTED = "REJECTED", _("Rejected")
     EXPIRED = "EXPIRED", _("Expired")
