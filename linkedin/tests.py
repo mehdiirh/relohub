56b459a725f323870cf81705a7c71bcc6fd924ed
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-04-28 19:27:01 +0330

    [UPDATE] added tests for HTTPProxy model

--- linkedin/tests.py
+++ linkedin/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
 
-# Create your tests here.
+from linkedin.models import HTTPProxy
+
+
+class HTTPProxyModelTest(TestCase):
+
+    def test_link_property_without_username_and_password(self):
+        proxy = HTTPProxy(host="example.com", port=8080)
+        self.assertEqual(
+            proxy.link,
+            "http://example.com:8080",
+        )
+
+    def test_link_property_with_username_and_password(self):
+        proxy = HTTPProxy(
+            host="example.com", port=8080, username="user", password="pass"
+        )
+        self.assertEqual(
+            proxy.link,
+            "http://user:pass@example.com:8080",
+        )
+
+    def test_link_dict_property_without_username_and_password(self):
+        proxy = HTTPProxy(host="example.com", port=8080)
+        self.assertEqual(
+            proxy.link_dict,
+            {
+                "http": "http://example.com:8080",
+                "https": "http://example.com:8080",
+            },
+        )
+
+    def test_link_dict_property_with_username_and_password(self):
+        proxy = HTTPProxy(
+            host="example.com", port=8080, username="user", password="pass"
+        )
+        self.assertEqual(
+            proxy.link_dict,
+            {
+                "http": "http://user:pass@example.com:8080",
+                "https": "http://user:pass@example.com:8080",
+            },
+        )
