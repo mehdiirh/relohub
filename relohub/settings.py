35917f951e48d84184e69c15e209fd2fbe0f8f34
Mehdi Rahimi <mehdii.rh78@gmail.com>
2024-04-30 14:21:02 +0330

    [ADD] ignore docker.env file

--- relohub/settings.py
+++ relohub/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "core.middlewares.force_change_password.ForceDefaultAdminToChangePassword",
+    "core.middlewares.force_change_password.ForceDefaultAdminToChangePassword",
 ]
 
 ROOT_URLCONF = "relohub.urls"
